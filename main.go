83b7bdb6e41f5e954e522134d324e70a1cfa28d1
Kevin Klopfenstein <kk@sudo-i.net>
2024-06-04 20:44:36 +0200

    update

--- main.go
+++ main.go
@@ -47,6 +47,7 @@ func main() {
 		Description: "IP Toolbox",
 		Styles: []string{
 			"https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css",
+			"/web/custom.css",
 		},
 		Body: getBody,
 		HTML: getHtml,
